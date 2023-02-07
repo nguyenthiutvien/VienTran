dc67b7d65c3d2cba4314052ac29d7735d6dcae34
Ho Thi Thu Tran <116275970+HuynhThiMyHanh@users.noreply.github.com>
2023-02-07 11:52:05 +0700

    Tran continue finished

--- test.php
+++ test.php
@@ -5,7 +5,28 @@
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<title>Document</title>
+	<style>
+		.hv1{
+			width: 20px;
+			height: 20px;
+			background: blue;
+			float: left;
+			margin-bottom: 5px;
+			margin-left: 5px;
+			border: 1px solid black;
+		}
+		.hv2{
+			width: 20px;
+			height: 20px;
+			background:red;
+			float: left;
+			margin-bottom: 5px;
+			margin-left: 5px;
+			border: 1px solid black;
+		}
+	</style>
 </head>
+
 <body>
 	
 	<?php
